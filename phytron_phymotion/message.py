92f4a74a1b29c483e35665fb996e875abd9be8e9
TUM-E21-ThinFilms <alexander.book@frm2.tum.de>
2016-11-14 11:14:22 +0100

    Adding response class

--- phytron_phymotion/message.py
+++ phytron_phymotion/message.py
@@ -13,18 +13,25 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+def compute_chksum(data):
+    chksum = 0
+    for el in data:
+        chksum = ord(el) ^ chksum
+          
+    return hex(chksum)[2:].upper().zfill(2)
+
 class Message(object):
     SEPARATOR = ':'
     STX = chr(0x02)
     ETX = chr(0x03)
     
     def __init__(self):
-        self.addr = 0
+        self.addr = '0'
         self.cmd = ''
         self.chksum = 'XX'
     
     def set_cmd(self, cmd):
-        self.cmd = cmd
+        self.cmd = str(cmd)
         
     def get_cmd(self):
         return self.cmd
@@ -36,30 +43,103 @@ class Message(object):
         return self.chksum
     
     def compute_checksum(self):
-        data = list("".join([self.addr, self.cmd, self.SEPARATOR]))
-        
-        chksum = 0
-        for el in data:
-            chksum = ord(el) ^ chksum
-            
-        return hex(chksum)[2:]
+        return compute_chksum(list("".join([self.addr, self.cmd, self.SEPARATOR])))
         
     def set_address(self, addr):
-        self.addr = addr & 0xF
+        if isinstance(addr, (int, long)):
+            if addr in range(0, 16):
+                addr = hex(addr)[2:]
+            else:
+                raise ValueError("address must be either 0...15 or '@'")
+
+        if not isinstance(addr, basestring):
+            raise ValueError("address must either be an integer or a string")
+                
+        if addr.upper() in "1234567890ABCEDF@" and len(addr) == 1:
+            self.addr = addr.upper()
+        else:
+            raise ValueError("address must be either 0...9, A...F or '@'")
     
     def get_address(self):
         return self.addr
     
     def get_raw(self):
-        return "".join([self.STX, self.cmd, self.SEPARATOR, self.chksum, self.EXT])
+        return "".join([self.STX, self.addr, self.cmd, self.SEPARATOR, self.chksum, self.ETX])
     
     def __str__(self):
-	    pass
+	    return self.get_raw()
 
     def __repr__(self):
 	    return self.__str__()
     
-
 class Response(object):
+    
+    ACK = chr(0x06)
+    NAK = chr(0x15)
+    
     def __init__(self, response_array):
+        
+        response_array = str(response_array)
+        
+        if len(response_array) > 5:
+            self.stx = response_array[0]
+            self.status = response_array[1]
+            self.etx = response_array[-1]
+            self.chksum = response_array[-3:-1]
+            
+            if response_array[2] == Message.SEPARATOR:
+                self.response = ''
+            elif response_array[-4] == Message.SEPARATOR:
+                self.response = response_array[2:-4]
+            else:
+                raise ValueError("Invalid response given (Separator ':' not found)")
+        else:
+            raise ValueError("Invalid response given (Response too short)")
+
+    def get_response(self):
+        return self.response
+    
+    def get_raw(self):
+        return "".join([self.stx, self.status, self.response, Message.SEPARATOR, self.chksum, self.etx])
+    
+    def compute_checksum(self):
+        return compute_chksum(list("".join([self.stx, self.status, self.response, Message.SEPARATOR])))
+    
+    def is_successful(self):
+        return self.status == self.ACK
+    
+    def get_checksum(self):
+        return self.chksum
+    
+    def is_valid(self):
+        if self.stx is not Message.STX or self.etx is not Message.ETX:
+            return False
+        
+        if not self.compute_checksum() == self.chksum:
+            return False
+        
+        return True     
+    
+class AbstractMessage(object):
+    def __init__(self, msg):
+        self.msg = Message()
+        self.init()
+        
+    def init(self):
         pass
+        
+    def get_message(self):
+        return self.msg
+    
+    def create_response(self, response):
+        raise NotImplementedError()
+    
+class AbstractResponse(object):
+    def __init__(self, response):
+        if not isinstance(response, Response):
+            raise TypeError("No Response given")
+            
+        self.resp = response
+        
+    def get_response(self):
+        return self.response
