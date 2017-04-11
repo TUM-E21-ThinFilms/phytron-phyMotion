3a1ccd918547479a96bf14c86662692b206c2772
Alexander Book <alexander.book@frm2.tum.de>
2017-04-11 10:39:12 +0200

    Changing logging

--- phytron_phymotion/factory.py
+++ phytron_phymotion/factory.py
@@ -13,23 +13,15 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import logging
-
 from e21_util.transport import Serial
+from e21_util.logging import get_sputter_logger
 from protocol import PhytronProtocol
 from driver import PhytronDriver
 
 class PhytronFactory:
     
     def get_logger(self):
-        logger = logging.getLogger('Phytron phyMotion')
-        logger.setLevel(logging.DEBUG)
-        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-        fh = logging.FileHandler('phytron.log')
-        fh.setLevel(logging.DEBUG)
-        fh.setFormatter(formatter)
-        logger.addHandler(fh)
-        return logger
+        return get_sputter_logger('Phytron phyMotion', 'phytron.log')
     
     def create_sputter(self, device='/dev/ttyUSB00', logger=None):
         if logger is None:
